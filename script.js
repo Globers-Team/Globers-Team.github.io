b9780d2f922578c353d25468227b5d90f81b3aff
CatPC-dot <mak303nospam@gmail.com>
2026-01-18 21:37:31 +0100

    t6ffi

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let val = true;
-function toggleDarkMode() {
-  var element = document.body;
+
+function themeButton() {
   val = !val
+  setTheme();
+}
+
+function setTheme() {
+  var element = document.body;
+  var col = document.getElementById("games");
+  element.classList.toggle("dark-mode");
   if (val) {
-    element.classList.toggle("dark-mode");
+
+    col.style.backgroundColor = "#3b3b3bff";
+    col.style.color = "#ffffffff"
   }
   else {
-    element.classList.toggle("dark-mode");
+
+    col.style.backgroundColor = "#d8d8d8ff";
+    col.style.color = "#000000ff"
   }
 }
