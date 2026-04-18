85faf06cf7f37a5c00f13fa7d9f5aa896c34a869
CatPC-dot <mak303nospam@gmail.com>
2026-04-18 21:11:34 +0200

    polska placowka

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function themeButton() {
 function setTheme() {
   var element = document.body;
   var col = document.getElementById("games");
+  var col2 = document.getElementById("servers");
   element.classList.toggle("dark-mode");
   if (val) {
-
     col.style.backgroundColor = "#3b3b3bff";
     col.style.color = "#ffffffff"
+    col2.style.backgroundColor = "#3b3b3bff";
+    col2.style.color = "#ffffffff"
   }
   else {
-
-    col.style.backgroundColor = "#d8d8d8ff";
-    col.style.color = "#000000ff"
+    col.style.backgroundColor = "#3b3b3bff";
+    col.style.color = "#ffffffff"
+    col2.style.backgroundColor = "#d8d8d8ff";
+    col2.style.color = "#000000ff"
   }
 }
